03738dc66b33cf0f1d37fa041f35e78d93fc2681
Felipe <dasilva.felipex93@gmail.com>
2021-07-05 10:25:23 -0300

    chapter 02 - 'templates'

--- app/ext/site/views.py
+++ app/ext/site/views.py
@@ -1,8 +1,25 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint("site", __name__)
 
 
 @bp.route("/")
 def index():
-    return "Hello World!"
+    title = "Home"
+    user = {"name": "Felipe"}
+    posts = [
+        {
+            "author": {"username": "Felipe"},
+            "body": "Beautiful day in Portland",
+        },
+        {
+            "author": {"username": "Eliza"},
+            "body": "Avengers movie was so cool!",
+        },
+        {
+            "author": {"username": "Matteo"},
+            "body": "I love mom and daddy",
+        },
+    ]
+
+    return render_template("index.html", title=title, user=user, posts=posts)
