977b17a3fe1f3df28f9dd3fb7bcb1bf24cb54933
Felipe <dasilva.felipex93@gmail.com>
2021-07-05 09:18:30 -0300

    chapter 01 - 'Hello World app!'

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+from app.ext import site
+
+
+def create_app():
+    app = Flask(__name__)
+
+    site.init_app(app)
+
+    return app
---
+++ app/ext/site/views.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+
+bp = Blueprint("site", __name__)
+
+
+@bp.route("/")
+def index():
+    return "Hello World!"
